069556f78704a2e4f6a26089624ee2b6a0281235
Javier G. Sogo <jgsogo@gmail.com>
2019-07-22 09:48:01 +0200

    other strategy for protobuffers

--- conanfile.py
+++ conanfile.py
@@ -1,3 +1,4 @@
+import os
 from conans import ConanFile, CMake
 
 class CoreMessages(ConanFile):
@@ -19,6 +20,16 @@ class CoreMessages(ConanFile):
 
     def build_requirements(self):
         self.build_requires("protoc_installer/3.6.1@bincrafters/stable")
+
+    def source(self):
+        # Generate protobuf messages
+        message_folder = os.path.join(os.path.dirname(__file__), "messages")
+        self.output.info("message_folder: {}".format(message_folder))
+        messages = [os.path.join(message_folder, it) for it in os.listdir(message_folder) if it.endswith(".proto")]
+        command = "protoc --proto_path={}".format(message_folder)
+        command += " --cpp_out={}".format(message_folder)
+        command += " {}".format(" ".join(messages))
+        self.run(command)
 
     def build(self):
         cmake = CMake(self)
